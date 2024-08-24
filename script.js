a21fd87f7b700068c2e8930f547e510d71f0164e
ricMejia1 <81607366+ricMejia1@users.noreply.github.com>
2024-08-24 13:47:20 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('nav ul');
+
+navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
